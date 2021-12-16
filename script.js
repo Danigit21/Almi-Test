b7cbe78398b5dc9e307fd800199e6406ae964492
Danib <danib@hotmail.com>
2021-12-16 13:13:04 +0100

    testing

---
+++ script.js
@@ -0,0 +1,17 @@
+let mobileLinksEl = document.querySelector("#mobileLinks")
+let hamButtonEl = document.querySelector("#button")
+
+const myFunction = () => {
+    if (mobileLinksEl.style.display === "block") {
+        mobileLinksEl.style.display = "none";
+    } else {
+        mobileLinksEl.style.display = "block";
+    }
+
+    if (hamButtonEl.classList.contains("fa-bars")) {
+        hamButtonEl.classList.replace("fa-bars", "fa-times");
+    
+      } else {
+        hamButtonEl.classList.replace("fa-times", "fa-bars");
+      }
+}
