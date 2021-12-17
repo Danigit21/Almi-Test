d4321d7f874df7689ec0f1b68e45dea88d6f32f5
Danib <danib@hotmail.com>
2021-12-17 14:29:10 +0100

    Final update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let mobileLinksEl = document.querySelector("#mobileLinks")
-let hamButtonEl = document.querySelector("#button")
+let hamButtonEl = document.querySelector("#ham-button")
 
 const myFunction = () => {
     if (mobileLinksEl.style.display === "block") {
@@ -11,8 +11,10 @@ const myFunction = () => {
 
     if (hamButtonEl.classList.contains("fa-bars")) {
       hamButtonEl.classList.replace("fa-bars", "fa-times");
+      hamButtonEl.classList.add("white")
     
     } else {
       hamButtonEl.classList.replace("fa-times", "fa-bars");
+      hamButtonEl.classList.remove("white")
     }
 }
